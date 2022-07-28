b73ec84f1c33ab4861a4d0b534ac999f86913054
madhums0411 <madhums41175@gmail.com>
2022-07-28 23:12:54 +0530

    Create a simple Linked List

--- DataStructure/DataStructure/Program.cs
+++ DataStructure/DataStructure/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Welcome to Linked List Data Structure Problem");
+﻿using DataStructureProblem;
+
+LinkedLists linkedLists = new LinkedLists();
+
+Node node1 = new Node(56);
+Node node2 = new Node(30);
+Node node3 = new Node(70);
+node1.next = node2;
+node2.next = node3;
+linkedLists.Head = node1;
+linkedLists.Display();
