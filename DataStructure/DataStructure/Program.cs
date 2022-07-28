b30d8fae5c2fedc8badcfe48cef2067fac9e4bd7
madhums0411 <madhums41175@gmail.com>
2022-07-28 23:21:25 +0530

    Added Node

--- DataStructure/DataStructure/Program.cs
+++ DataStructure/DataStructure/Program.cs
@@ -2,10 +2,7 @@
 
 LinkedLists linkedLists = new LinkedLists();
 
-Node node1 = new Node(56);
-Node node2 = new Node(30);
-Node node3 = new Node(70);
-node1.next = node2;
-node2.next = node3;
-linkedLists.Head = node1;
+linkedLists.AddNode(70);
+linkedLists.AddNode(30);
+linkedLists.AddNode(56);
 linkedLists.Display();
