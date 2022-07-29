253a7458d9cb2262c41ab2f27ceee1e38c6da73b
madhums0411 <madhums41175@gmail.com>
2022-07-29 19:09:06 +0530

    Insert node value to LinkedList

--- DataStructure/DataStructure/Program.cs
+++ DataStructure/DataStructure/Program.cs
@@ -2,15 +2,9 @@
 
 LinkedLists linkedLists = new LinkedLists();
 
-Node node1 = new Node(56);
-Node node2 = new Node(30);
-Node node3 = new Node(70);
-node1.next = node2;
-node2.next = node3;
-linkedLists.Head = node1;
-Console.WriteLine("Search For Node value 30");
+Console.WriteLine("Add Node After Node value 30 ");
+linkedLists.AppendNode(56);
+linkedLists.AppendNode(30);
+linkedLists.AppendNode(70);
+linkedLists.InsertNode(40, 30);
 linkedLists.Display();
-if (linkedLists.Search(30))
-{
-    Console.WriteLine("\nNode with value 30 is available in Linked list");
-}
