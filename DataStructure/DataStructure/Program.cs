1df879df0197ca615f7e3a8b84da49920e7bb968
madhums0411 <madhums41175@gmail.com>
2022-07-29 18:33:38 +0530

    Delete the last element

--- DataStructure/DataStructure/Program.cs
+++ DataStructure/DataStructure/Program.cs
@@ -5,6 +5,6 @@ LinkedLists linkedLists = new LinkedLists();
 linkedLists.AppendNode(56);
 linkedLists.AppendNode(30);
 linkedLists.AppendNode(70);
-Console.WriteLine("Delete First element");
-linkedLists.DeleteNodeAtFirst();
+Console.WriteLine("Delete Last element");
+linkedLists.DeleteNodeAtLast();
 linkedLists.Display();
